7ad1f6468ade0bc58bc4b5791e7f69ef9265a96d
hpasarin <hpasarin@LES004648.corp.capgemini.com>
2018-01-31 13:22:05 +0100

    se quita el interfaz

--- src/com/hector/operaciones/Lanzador.java
+++ src/com/hector/operaciones/Lanzador.java
@@ -1,18 +1,11 @@
 package com.hector.operaciones;
 
-import javax.swing.JOptionPane;
-
 public class Lanzador {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		
-		
-		
-		JOptionPane panelInf = new JOptionPane();
-		panelInf.setMessage("este mes tienes.");
-		panelInf.showMessageDialog(null,"aasdfasdfasdf");
 		Meses meses= new Meses();
 		meses.numeroDiasMesActual();
 	}
